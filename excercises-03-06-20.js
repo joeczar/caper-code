ccd6a6ded4f4ba1f8953e03d9a45f8beb4a20ce1
Joe Czarnecki <joe.czar@outlook.com>
2020-06-03 13:34:30 +0200

    excercise for 03/06/2020

---
+++ excercises-03-06-20.js
@@ -0,0 +1,53 @@
+function logType(input) {
+    var type = typeof input;
+
+    function log(data) {
+        console.log('"' + data + '!' + '"');
+    }
+
+    if (input === null) {
+        log(input);
+    } else if (input === undefined) {
+        log(input);
+    } else if (Number.isNaN(input)) {
+        log(input);
+    } else if (type === 'number') {
+        log(type);
+    } else if (type === 'string') {
+        log(type);
+    } else if (type === 'boolean') {
+        log(type);
+    } else if (type === 'bigint') {
+        log(type);
+    } else if (type === 'function') {
+        log(type);
+    } else if (Array.isArray(input)) {
+        log('array');
+    } else if (type === 'object') {
+        log(type);
+    } else if (type === 'symbol') {
+        log('I have no idea');
+    }
+}
+
+logType();
+
+var a = {
+    Berlin: 'Germany',
+    Paris: 'France',
+    'New York': 'USA',
+};
+
+var b = {};
+
+function keyValueSwitcher(obj1, obj2) {
+    // take an object and creat a new object with keys & values switched
+    for (key in obj1) {
+        obj2[obj1[key]] = key;
+    }
+}
+keyValueSwitcher(a, b);
+
+for (var i = 10; i > 0; i--) {
+    console.log(i);
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+var x = [];
+
+if (x.length) {
+    console.log('yes!');
+} else {
+    console.log('no!');
+}
