e5ae9ed17afa566da5680cbe5a7665b39768d39d
Joe Czarnecki <joe.czar@outlook.com>
2020-06-10 14:40:52 +0200

    finished basic ticker

--- exercises/ticker/script.js
+++ exercises/ticker/script.js
@@ -26,22 +26,35 @@
     Call your animation function to kick the whole thing off
 
 */
-
-(window.onload = function () {
+(function () {
     var headlines = document.getElementById('headlines');
     var links = headlines.getElementsByTagName('A');
     var left = headlines.offsetLeft;
+    var step;
+
+
+
+    headlines.addEventListener('mouseover', function (e) {
+         e.target.style.cssText =
+             'color:red;';
+        cancelAnimationFrame(step);
+    });
+    headlines.addEventListener('mouseout', function(e){
+         e.target.style.cssText = 'textDecoration:none;color:white;'
+         window.requestAnimationFrame(moveHeadlines);
+    })
 
     function moveHeadlines() {
         left--;
-        console.log(left, -links[0].offsetWidth);
-        if (left <= -links[0].offsetWidth) {
+        // console.log(left, links[0].offsetWidth);
+        if (left < -links[0].offsetWidth) {
             left += links[0].offsetWidth;
+            headlines.style.left = left + 'px';
             headlines.appendChild(links[0]);
         }
         headlines.style.left = left + 'px';
 
-        window.requestAnimationFrame(moveHeadlines);
+       step = window.requestAnimationFrame(moveHeadlines);
     }
-    window.requestAnimationFrame(moveHeadlines);
+    step = window.requestAnimationFrame(moveHeadlines);
 })();
