e3169d2ac738d8594b1879d62228218709122474
Joe Czarnecki <joe.czar@outlook.com>
2020-06-17 12:39:32 +0200

    first draft finished

--- exercises/incSearch/script.js
+++ exercises/incSearch/script.js
@@ -24,62 +24,63 @@
     });
 
     // elements
-    var doc = $(document)
+    var doc = $(document);
     var input = $('input');
     var resultsContainer = $('#resultsContainer');
-    var elems = resultsContainer.find('p');
+    var elems;
+    var highlighted;
     // counter
     var selector = 0;
-    
+
     // event listeners
     // input
     input.on('input', function (e) {
         var inputVal = input.val();
-        getCountries(inputVal)
+        getCountries(inputVal);
         //console.log(list);
-        
     });
     input.on('focus', function (e) {
         input.css({
             border: '2px solid blue',
         });
+        var inputVal = input.val();
+        getCountries(inputVal);
     });
     input.on('blur', function (e) {
         input.css({
             border: '2px solid white',
         });
+        resultsContainer.html('');
     });
 
     // resultsContainer
-    resultsContainer.on('mouseover', 'p', function(e) {
-       var p = $(e.target)
-       p.addClass('highlight')
-       
-    })
+    resultsContainer.on('mouseover', 'p', function (e) {
+        var p = $(e.target);
+        p.addClass('highlight');
+    });
     resultsContainer.on('mouseout', 'p', function (e) {
         var p = $(e.target);
         p.removeClass('highlight');
     });
-    resultsContainer.on('click', 'p', function (e) {
+    resultsContainer.on('mousedown', 'p', function (e) {
         var p = $(e.target);
         var text = p.text();
-        input.val(text)
+        input.val(text);
+        
     });
-    
-    doc.on('keydown', function(e) { 
-         var elems = resultsContainer.find('p');
-         
-         
-         handleKeydown(e, elems); 
-    })
-    
+
+    doc.on('keydown', function (e) {
+        
+            handleKeydown(e, elems);
+        
+    });
 
     // search
     function getCountries(input) {
-        var filtered = countries.filter(function(country) {
-            return indexPass(country, input)
+        var filtered = countries.filter(function (country) {
+            return indexPass(country, input);
         });
-        var topFour = filtered.slice(0, 4);
+        var topFour = filtered.length === 0 ? ['No Results'] : filtered.slice(0, 4);
 
         function indexPass(value, input) {
             return (
@@ -87,26 +88,27 @@
                 input.length > 0
             );
         }
-        var resultsHTML = "";
+        var resultsHTML = '';
         
-        topFour.forEach(function(result) {
-            resultsHTML += '<p class="country">' + result + '</p>'
-        })
+        topFour.forEach(function (result) {
+            resultsHTML += '<p class="country">' + result + '</p>';
+        });
         resultsContainer.html(resultsHTML);
-        
     }
 
     // up down function
     function handleKeydown(e, elems) {
-        // increment selector        
+        elems = resultsContainer.find('p');
+        highlighted = $('.highlight');
+
+        // increment selector
         if (e.keyCode === 40) {
-            if (selector > elems.length -1) {
+            if (selector > elems.length - 1) {
                 selector = 0;
             }
             selector++;
-            
         } else if (e.keyCode === 38) {
-            selector--
+            selector--;
             if (selector < 0) {
                 selector = 3;
             }
@@ -116,19 +118,16 @@
             if (i === selector && !elems.eq(i).hasClass('highlight')) {
                 elems.eq(i).addClass('highlight');
             } else {
+                
                 elems.eq(i).removeClass('highlight');
             }
         }
 
         // enter
-       
-        if (e.keyCode === 13) {
-            console.log($('.highlight').text());
-            
-            
-            input.val($('.highlight').text());
-        }
-        
-    }
 
+        if (e.keyCode === 13) {
+            input.val(highlighted.text());
+            resultsContainer.html('');
+        }
+    }
 })();
