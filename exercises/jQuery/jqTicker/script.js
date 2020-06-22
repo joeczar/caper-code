fc05eb5141ae835fc3de4391b5ec454ac5244139
Joe Czarnecki <joe.czar@outlook.com>
2020-06-22 18:15:01 +0200

    finished ajax ticker

--- exercises/jQuery/jqTicker/script.js
+++ exercises/jQuery/jqTicker/script.js
@@ -1,62 +1,83 @@
-(function() {
-    // Top Ticker
-    var headlines = $('#headlines');
-    var links = headlines.find('A');
-    var left = headlines.offset().left;
+(function () {
+    // Get links
+    $.ajax({
+        url: '/links.json',
+        method: 'GET',
+        success: function (response) {
+            var html = '';
+            for (var i = 0; i < response.length; i++) {
+                var link =
+                    '<a id="link_' +
+                    i +
+                    ' href="' +
+                    response[i].link +
+                    '" >⛧ ' +
+                    response[i].headline +
+                    ' ⛧</a>';
+                html += link;
+            }
+            $('#headlines').html(html);
+            tick()
+            
+        },
+        error: function (err) {
+            console.log('Ajax Error:', err);
+        },
+    });
 
-    // Bottom Ticker
-    var bHeadlines = $('#bottomHeadlines');
-    var bLinks = bHeadlines.find('A');
-    var bLeft = bHeadlines.offset().left;
-    
-    var lastLink = bLinks.length - 1;
-    var wWidth = $(window).width()
+    function tick() {
+        var headlines = $('#headlines');
+        var links = headlines.find('a')
+        var left = headlines.offset().left;
 
-    var pentagram = $('#pentagram')
-    var step;
-    var degrees = 0;
+        // Bottom Ticker
+        var bHeadlines = $('#bottomHeadlines');
+        var bLinks = bHeadlines.find('A');
+        var bLeft = bHeadlines.offset().left;
 
-    // top event listeners
-    headlines.on('mouseover', function(e) { 
-        $(e.target).css({ color: 'red', })
-        cancelAnimationFrame(step);
-    })
-    headlines.on('mouseout', function(e) {
-        $(e.target).css({ 
-            textDecoration: 'none',
-            color: 'white', 
+        var lastLink = bLinks.length - 1;
+        var wWidth = $(window).width();
+
+        var pentagram = $('#pentagram');
+        var step;
+        var degrees = 0;
+
+        // top event listeners
+        headlines.on('mouseover', function (e) {
+            $(e.target).css({ color: 'red' });
+            cancelAnimationFrame(step);
         });
-        window.requestAnimationFrame(moveHeadlines);
-    })
-    
-    function moveHeadlines() {
-        left--;
-        bLeft++;
-        // move top headlines
-        if (left <= -links.eq(0).outerWidth()){
-            left += links.eq(0).outerWidth();
-            headlines.css({ left: left + 'px' })
-            headlines.append(links.eq(0));
-           
-        }
-        // move bottom headlines
-        // get last link
-        console.log((bHeadlines.width()) - wWidth);
-        if (bLeft >= bLinks.eq(lastLink).outerWidth()) {
-            bLeft -= bLinks.eq(lastLink).outerWidth();
-            bHeadlines.css({
-                left: bLeft + 'px',
+        headlines.on('mouseout', function (e) {
+            $(e.target).css({
+                textDecoration: 'none',
+                color: 'white',
             });
-            bHeadlines.prepend(bLinks.eq(lastLink))
-        }
-        
-       
-        bHeadlines.css({
-            left: bLeft + 'px',
+            window.requestAnimationFrame(moveHeadlines);
         });
-        headlines.css({'left': left + 'px' });
+
+        var count = 0;
+        function moveHeadlines() {
+            left--;
+
+            var width = links.eq(count).outerWidth();
+
+            // move top headlines
+            if (left <= -width) {
+                left += width;
+                // headlines.css({ left: left + 'px' })
+                
+                headlines.append(links.eq(count));
+                count++;
+                if (count > links.length - 1) {
+                    count = 0;
+                }
+            }
+            // move bottom headlines
+            // get last link
+
+            headlines.css({ left: left + 'px' });
+            step = window.requestAnimationFrame(moveHeadlines);
+        }
         step = window.requestAnimationFrame(moveHeadlines);
     }
-    step = window.requestAnimationFrame(moveHeadlines);
-    
 })();
