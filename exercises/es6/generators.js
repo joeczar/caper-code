5be8ce216b7da6b904792a50aaf5ed5ccbdd35c9
Joe Czarnecki <joe.czar@outlook.com>
2020-06-25 16:04:14 +0200

    Finished generator exercises

--- exercises/es6/generators.js
+++ exercises/es6/generators.js
@@ -18,16 +18,12 @@ for (const num of generateFizzBuzz()) {
 }
 
 function* reverseGenerator(arr) {
+    const popme = [...arr];
     for (let i = 0; i < arr.length; i++) {
         if (arr.length == 0) {
-            console.log("done");
-
             return { done: true };
         } else {
-            var popped = arr.pop();
-            console.log({ arr, popped, length: arr.length });
-
-            yield popped;
+            yield popme.pop();
         }
     }
 }
@@ -37,3 +33,23 @@ const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 for (const elem of reverseGenerator(nums)) {
     console.log({ elem });
 }
+
+function makeWierdArray() {
+    const arr = [...arguments];
+
+    arr[Symbol.iterator] = function* () {
+        // I tried .reverse() but it wouldn't wouk...
+        const reversed = [];
+        for (let i = arr.length - 1; i >= 0; i--) {
+            const elem = arr[i];
+            reversed.push(elem);
+        }
+
+        yield reversed;
+    };
+    return arr;
+}
+const wierd = makeWierdArray(10, 20, 30);
+const wierder = [...wierd];
+console.log(wierd);
+console.log(wierder);
