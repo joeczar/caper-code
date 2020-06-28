66aead8458b535522f1aae5aa85c7a4be553993e
Joe Czarnecki <joe.czar@outlook.com>
2020-06-28 18:44:54 +0200

    Finished countdown - for real

--- exercises/node/eventEmitters/countdown/countdown.js
+++ exercises/node/eventEmitters/countdown/countdown.js
@@ -2,31 +2,24 @@
     When a countdown instance is created, it should emit a secondElapsed event every second until it gets to zero, passing the current number of seconds remaining to any listeners that have been added
 */
 
-const events = require('events');
-const { appendFileSync } = require('fs');
+const events = require("events");
+
 module.exports = class Countdown extends events.EventEmitter {
     constructor(num) {
         super();
         this.timeLeft = num;
-        this.interval = function() {
-            
-            const int = setInterval(() => {                
+
+        this.on("secondElapsed", this.interval);
+        this.emit("secondElapsed", this.timeLeft);
+    }
+    interval() {
+        const int = setInterval(() => {
             if (this.timeLeft < 0) {
                 clearInterval(int);
                 return 0;
             }
-            this.emit('secondElapsed', this.timeLeft);
+            this.emit("secondElapsed", this.timeLeft);
             this.timeLeft--;
         }, 1000);
     }
-        this.on('secondElapsed', this.interval);
-    }
 };
-    
-    
-
-    
-
-function taskFunc() {
-     
-}
--- exercises/node/eventEmitters/countdown/index.js
+++ exercises/node/eventEmitters/countdown/index.js
@@ -1,14 +1,11 @@
-const Countdown = require('./countdown');
+const Countdown = require("./countdown");
 
 const countdown = new Countdown(10);
-// console.log(countdown);
 
-countdown.on('secondElapsed', function (timeLeft) {
-        
+countdown.on("secondElapsed", function (timeLeft) {
     if (timeLeft > 0) {
         console.log(timeLeft);
     } else {
-        console.log('lift off!');
+        console.log("lift off!");
     }
 });
-countdown.emit('secondElapsed', countdown.interval)
