5ab7ad9f049749fdd68d9e20bf460d2f12cb64ed
Joe Czarnecki <joe.czar@outlook.com>
2020-07-06 16:37:20 +0200

    finished handlebars

--- exercises/node/handlebars-project/getProjects.js
+++ exercises/node/handlebars-project/getProjects.js
@@ -1,18 +1,18 @@
 module.exports.getProjectObj = function (filepath) {
-    const fs = require("fs");
-    const path = require("path");
+    const fs = require('fs');
+    const path = require('path');
 
     const projectArr = [];
 
     const files = fs.readdirSync(filepath, { withFileTypes: true });
 
     files.forEach((file) => {
-        const nextPath = filepath + "/" + file.name;
+        const nextPath = filepath + '/' + file.name;
         const stat = fs.statSync(nextPath);
         if (stat.isDirectory()) {
             const files = fs.readdirSync(nextPath, { withFileTypes: true });
             const screenshot = files.filter(
-                (file) => path.extname(file.name) === ".png"
+                (file) => path.extname(file.name) === '.png'
             )[0];
 
             const obj = {
@@ -20,7 +20,7 @@ module.exports.getProjectObj = function (filepath) {
                 directory: file.name,
                 img: {
                     name: parseProjectName(screenshot.name),
-                    path: file.name + "/" + screenshot.name,
+                    path: file.name + '/' + screenshot.name,
                 },
             };
             projectArr.push(obj);
@@ -28,12 +28,25 @@ module.exports.getProjectObj = function (filepath) {
     });
 
     function parseProjectName(name) {
-        name = name.split(".")[0];
+        name = name.split('.')[0];
         return name
-            .split("-")
+            .split('-')
             .map((elem) => elem.charAt(0).toUpperCase() + elem.slice(1))
-            .join(" ");
+            .join(' ');
     }
+
+    fs.writeFile(
+        './projects/projectData.json',
+        JSON.stringify(projectArr, null, 4),
+        function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            // console.log('The file was saved!');
+        }
+    );
+
+    // Or
 
     return projectArr;
 };
--- exercises/node/handlebars-project/index.js
+++ exercises/node/handlebars-project/index.js
@@ -1,34 +1,52 @@
-const express = require("express");
-
+const express = require('express');
+const projects = require('./projects/projectData.json');
 const app = express();
 const port = 8080;
 
-const hb = require("express-handlebars");
-app.engine("handlebars", hb());
-app.set("view engine", "handlebars");
+const hb = require('express-handlebars');
+app.engine('handlebars', hb());
+app.set('view engine', 'handlebars');
+const helpers = require('handlebars-helpers')();
 
 // data
-const { getProjectObj } = require("./getProjects");
+//const { getProjectObj } = require("./getProjects");
 
-const projects = getProjectObj("./projects");
+//const projects = getProjectObj("./projects");
 
-app.use(express.static("./projects"));
-app.use(express.static("./public"));
+app.use(express.static('./projects'));
+app.use(express.static('./public'));
 
-app.get("/", (req, res) => {
-    res.render("home", {
-        title: "My Portfolio",
-        cohort: "Caper",
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'My Portfolio',
+        cohort: 'Caper',
         projects,
     });
 });
 
-app.get("/about", (req, res) => {
-    res.render("about", {
-        title: "About",
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
     });
 });
 
+app.get('/projects/:project', (req, res) => {
+    const { project } = req.params;
+    const selectedProject = projects.find((item) => item.directory === project);
+
+    if (!selectedProject) {
+        res.render('404', {
+            title: '404 not found',
+        });
+        // res.sendStatus(404);
+    } else {
+        res.render('description', {
+            title: selectedProject.name,
+            project: selectedProject,
+            projects,
+        });
+    }
+});
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}...`);
 });
