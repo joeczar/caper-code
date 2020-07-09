e4ae7b407095653d0cf1cb32d48a3a81a8becf55
Joe Czarnecki <joe.czar@outlook.com>
2020-07-09 17:41:36 +0200

    finished testing exercises

--- exercises/testing/testing/exercises/3/fn.js
+++ exercises/testing/testing/exercises/3/fn.js
@@ -1,2 +1,10 @@
-module.exports = function fn() {
+module.exports = function fn(arg) {
+    if (typeof arg === 'string') {
+        const result = arg.split('').reverse().join('');
+        return result;
+    } else if (typeof arg !== 'string' && !Array.isArray(arg)) {
+        return null;
+    } else {
+        return arg.map((elem) => fn(elem));
+    }
 };
--- exercises/testing/testing/exercises/3/fn.test.js
+++ exercises/testing/testing/exercises/3/fn.test.js
@@ -1,22 +1,13 @@
 const fn = require('./fn');
 
 test('a string as first argument', () => {
-    expect(
-        fn('Funky Chicken')
-    ).toBe('nekcihC yknuF');
+    expect(fn('Funky Chicken')).toBe('nekcihC yknuF');
 });
 
 test('neither a string nor an array as first argument', () => {
-    expect(
-        fn(90210)
-    ).toBe(null);
+    expect(fn(90210)).toBe(null);
 });
 
 test('an array as first argument', () => {
-    expect(
-        fn(['Funky Chicken', 90320])
-    ).toEqual([
-        'nekcihC yknuF',
-        null
-    ]);
+    expect(fn(['Funky Chicken', 90320])).toEqual(['nekcihC yknuF', null]);
 });
